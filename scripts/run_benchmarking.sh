35f60d10d338a6813f1fe18c066854f8057fce12
FlorianPO <florian.popek@gmail.com>
2016-02-03 12:26:26 +0100

    Changed script, for loops inverted

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,8 +4,10 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
-    for rep in `seq 1 5`; do
+for rep in `seq 1 30`; do
+	echo "Seq = $rep"
+    for i in 100 1000 10000 100000 1000000 2500000 5000000; do
+    	echo "i = $i"
         echo "Size: $i" >> $OUTPUT_FILE;
         ./src/parallelQuicksort $i >> $OUTPUT_FILE;
     done ;
